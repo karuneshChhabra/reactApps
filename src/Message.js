28e8b91f38a5657c037f9de5e36becc81325b446
karunesc <karunesh.chhabra@nokia.com>
2021-04-24 23:55:23 +0530

    first commit

---
+++ src/Message.js
@@ -0,0 +1,36 @@
+import React, {forwardRef} from "react";
+import { Card ,CardContent,Typography,makeStyles } from '@material-ui/core';
+import "./Message.css"
+
+
+
+
+const Message =  forwardRef(({message,userName},ref) => {
+
+    
+    const isUser = userName ===message.userName;
+    
+
+    return(
+           <div ref={ref} className={`message__text ${isUser && 'message__user'}`}>
+               
+
+                <Card>
+            <CardContent>
+                <Typography  variant="h5" component="h2" >
+                <b>  {!isUser && `${message.userName?message.userName:"Unknown User"}:`} </b>  {message.message}
+                </Typography>
+                </CardContent>
+
+               </Card>
+           </div>
+                    
+              
+                
+           
+    );    
+
+
+});
+
+export default Message;
